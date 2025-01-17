85a7a925054bd166f49df956a838e6888c01a62f
Roman Roznovsky <roman@workstreams.ai>
2025-01-17 19:18:55 +0100

    parse username and password, when present auth 200

--- src/api/auth/__tests__/router.test.ts
+++ src/api/auth/__tests__/router.test.ts
@@ -8,10 +8,33 @@ import { app } from "@/server";
 
 describe("Auth API endpoints", () => {
   describe("POST /auth", () => {
-    it("Should fail to authenticate and return 400", async () => {
+    it("400 / Should fail to authenticate with no username and password", async () => {
       const response = await request(app).post("/auth").send({});
       expect(response.statusCode).eql(400);
       expect(response.body).eql({});
+    });
+
+    it("400 / should fail to authenticate with wrong bodyd", async () => {
+      const response = await request(app).post("/auth").send("sometext");
+      expect(response.statusCode).eql(400);
+      expect(response.body).eql({});
+    });
+
+    it("200 / should authenticate with any username / password", async () => {
+      const payload = {
+        username: "any",
+        password: "any",
+      };
+      const response = await request(app).post("/auth").send(payload);
+      expect(response.statusCode).eql(200);
+      expect(response.body).eql({
+        meta: {
+          success: true,
+        },
+        data: {
+          token: "jwt-token",
+        },
+      });
     });
   });
 });
--- src/api/auth/router.ts
+++ src/api/auth/router.ts
@@ -2,6 +2,21 @@ import express, { type Router } from "express";
 import { StatusCodes } from "http-status-codes";
 
 export const authRouter: Router = express.Router();
+
 authRouter.post("/", (req, res) => {
-  return res.send(StatusCodes.BAD_REQUEST);
+  const username = req.body?.username;
+  const password = req.body?.password;
+
+  if (!username || !password) {
+    return res.send(StatusCodes.BAD_REQUEST);
+  }
+
+  return res.json({
+    meta: {
+      success: true,
+    },
+    data: {
+      token: "jwt-token",
+    },
+  });
 });
